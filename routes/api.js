495f14d506d25ab0d0b19296b6daeecec7f2d24b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-01-27 08:47:57 -0700

    Added post request

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,28 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
 const express = require('express');
+const router = express.Router();
+
+// GET Request that returns projectData
+router.get('/weather', (req, res) => {
+  res.send(projectData);
+});
+
+
+// POST Request that adds incoming data to projectData
+router.post('/weather', (req, res) => {
+  console.log(req.body);
+  res.send({
+    type: 'POST',
+    temperature: req.body.temperature,
+    date: req.body.date,
+    userResponse: req.body.userResponse
+  });
+  projectData.push(req.body);
+  console.log(projectData);
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+// // Setup empty JS object to act as endpoint for all routes
+// projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
+const routes = require('./routes/api');
 
 // Start up an instance of app
 const app = express();
@@ -11,12 +12,15 @@ const app = express();
 const bodyParser = require('body-parser');
 /* Middleware*/
 // Here we are configuring express to use body-parser as middle-ware.
-// 'use' method - allows us to connect packages to our project
+// 'use' - allows us to connect packages to our project
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // cors for accessing resources from remote hosts
 const cors = require('cors');
 app.use(cors());
+
+// Initialize routes
+app.use('/api', routes);
 
 // Initialize the main project folder
 app.use(express.static('website'));
@@ -24,20 +28,7 @@ app.use(express.static('website'));
 // Setup Server
 const port = process.env.port || 3000;
 
+// Listen for requests
 const server = app.listen(port, ()=> {
   console.log(`Running on localhost: ${port}`);
 });
-
-// GET Request that returns projectData
-app.get('/all', (req, res) => {
-  res.send(projectData);
-  console.log("GET Request");
-});
-
-// POST Request that adds incoming data to projectData
-
-// app.post('/all', (req, res) => {
-//   projectData.push(req.temparature);
-//   projectData.push(req.date);
-//   projectData.push(req.userResponse);
-// });
