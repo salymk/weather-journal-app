0d0be3bcb025f26493a79cfdaf04f05b61f22cf7
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-02-03 08:49:18 -0700

    Restarted project, added bootstrap

--- website/app.js
+++ website/app.js
@@ -1,7 +1,13 @@
 /* Global Variables */
-
 const url = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '4907ba32968f69e6ea7d3c1d52b828d5';
+
+const zipCode = document.querySelector('.zipcode');
+const journalSummary = document.querySelector('.journal-summary');
+const outputSection = document.querySelector('.outputSection');
+const formInput = document.querySelector('.form-input');
+const saveBtn = document.querySelector('.save');
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -9,12 +15,4 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 fetch(url + '80015' + ',us&appid=' + apiKey)
   .then( res => res.json())
-  .then( res => console.log(res));
-
-const testFunc = async () => {
-  const response = await fetch(url + '80015' + ',us&appid=' + apiKey);
-  const jsonResponse = await response.json();
-  console.log(jsonResponse)
-}
-
-testFunc();
+  .then( data => console.log(data));
