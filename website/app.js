a51ca1259ba1a94e730c19761f4a256008ef5796
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-02-05 10:20:08 -0700

    Redid fetch request

--- website/app.js
+++ website/app.js
@@ -2,37 +2,102 @@
 const url = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '4907ba32968f69e6ea7d3c1d52b828d5';
 
+
+// DOM Variables
 const zipCode = document.querySelector('.zipcode');
 const journalSummary = document.querySelector('.journal-summary');
 const outputSection = document.querySelector('.outputSection');
 const formInput = document.querySelector('.form-input');
 const saveBtn = document.querySelector('.save');
 
-
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'/'+ d.getDate()+'/'+ d.getFullYear();
 
-saveBtn.addEventListener('click', (e) => {
+// getWeather();
+
+// Function to get weather data from server
+async function getWeather(userZip) {
+  const response = await fetch(`${url}${userZip},us&appid=${apiKey}&units=imperial`);
+  const weather = await response.json();
+
+  // Create a new journal
+  const newJournal = createJournal(weather.main.temp, newDate);
+  // Add new journal to website
+  outputSection.appendChild(newJournal);
+  formInput.reset();
+
+  // console.log(weather);
+}
+
+// Function to create new journal entries
+function createJournal (temp, date) {
+  // Create Elements
+  const article = document.createElement('article');
+  const cardHeader = document.createElement('h4');
+  const cardBody = document.createElement('div');
+  const cardTitle = document.createElement('h5');
+  const cardSubTitle = document.createElement('h6');
+  const cardText = document.createElement('p');
+
+  // Add content to cards
+  cardHeader.textContent = `Zip: ${zipCode.value}`;
+  cardTitle.innerHTML = `Today's weather is: ${temp}<span style="color: black;">°F</span>`;
+  cardSubTitle.textContent = date;
+  cardText.textContent = journalSummary.value;
+
+  // Create the card
+  article.appendChild(cardHeader);
+  article.appendChild(cardBody);
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardSubTitle);
+  cardBody.appendChild(cardText);
+
+  // Add classes to the Elements
+  article.classList.add('card', 'bg-light', 'mb-3');
+  cardHeader.classList.add('card-header');
+  cardBody.classList.add('card-body', 'mb-0');
+  cardTitle.classList.add('card-title');
+  cardSubTitle.classList.add('card-subtitle', 'text-muted');
+  cardText.classList.add('card-text', 'mt-4');
+
+  return article;
+}
+
+// Start the process
+saveBtn.addEventListener('click', (e)=> {
   e.preventDefault();
-
-  fetch(`${url}${zipCode.value},us&appid=${apiKey}&units=imperial`)
-    .then( res => res.json())
-    .then( data => {
-      let newJournal =
-      `
-      <div class="card bg-light mb-3" style="max-width: 20rem;">
-        <div class="card-header">${zipCode.value}</div>
-        <div class="card-body">
-          <h4 class="card-title">Today's weather is: ${data.main.temp}</h4>
-          <h6 class="card-subtitle text-muted">${newDate}</h6>
-          <p class="card-text mt-3">${journalSummary.value}</p>
-        </div>
-      </div>
-      `;
-
-      outputSection.innerHTML = newJournal;
-
-      formInput.reset();
-    });
+  getWeather(zipCode.value)
+    .then(response => {
+      console.log('Zip Code Received!')
+    })
+    .catch(error => {
+        console.log('Error!');
+        console.error(error);
+      });
 });
+
+//
+// saveBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//
+//   fetch(`${url}${zipCode.value},us&appid=${apiKey}&units=imperial`)
+//     .then( res => res.json())
+//     .then( data => {
+//       let newJournal =
+//       `
+//       <div class="card bg-light mb-3" style="max-width: 20rem;">
+//         <div class="card-header">${zipCode.value}</div>
+//         <div class="card-body">
+//           <h4 class="card-title">Today's weather is: ${data.main.temp}</h4>
+//           <h6 class="card-subtitle text-muted">${newDate}</h6>
+//           <p class="card-text mt-3">${journalSummary.value}</p>
+//         </div>
+//       </div>
+//       `;
+//
+//       outputSection.innerHTML = newJournal;
+//
+//       formInput.reset();
+//     });
+// });
